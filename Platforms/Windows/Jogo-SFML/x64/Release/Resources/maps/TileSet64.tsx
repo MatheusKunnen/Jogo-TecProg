538cf8937a0382091bcb6698ef779356b6a3a140
MatheusKunnen <matheuskl@accionproactiva.com>
2019-11-24 16:00:20 -0300

    Windows Build

---
+++ Platforms/Windows/Jogo-SFML/x64/Release/Resources/maps/TileSet64.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="TileSet64" tilewidth="64" tileheight="64" tilecount="36" columns="4">
+ <image source="tileset64.png" width="256" height="576"/>
+ <tile id="0" type="solid"/>
+ <tile id="1" type="solid"/>
+ <tile id="2" type="solid"/>
+ <tile id="3" type="solid"/>
+ <tile id="4" type="solid"/>
+ <tile id="5" type="solid"/>
+ <tile id="6" type="solid"/>
+ <tile id="7" type="solid"/>
+ <tile id="8" type="solid"/>
+ <tile id="9" type="solid"/>
+ <tile id="10" type="solid"/>
+ <tile id="11" type="solid"/>
+ <tile id="12" type="solid"/>
+ <tile id="13" type="solid"/>
+ <tile id="14" type="solid"/>
+ <tile id="15" type="solid"/>
+ <tile id="16" type="solid"/>
+ <tile id="17" type="solid"/>
+ <tile id="18" type="solid"/>
+ <tile id="19" type="solid"/>
+ <tile id="20" type="solid"/>
+ <tile id="21" type="solid"/>
+ <tile id="22" type="solid"/>
+ <tile id="23" type="solid"/>
+ <tile id="24" type="solid"/>
+ <tile id="25" type="solid"/>
+ <tile id="26" type="solid"/>
+ <tile id="27" type="solid"/>
+ <tile id="28" type="solid"/>
+ <tile id="29" type="solid"/>
+ <tile id="30" type="solid"/>
+ <tile id="31" type="solid"/>
+ <tile id="32" type="solid"/>
+ <tile id="33" type="solid"/>
+ <tile id="34" type="solid"/>
+ <tile id="35" type="solid"/>
+</tileset>
